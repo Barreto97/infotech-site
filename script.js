7bd736e8dbb1158c4cc97d9eede66c902028f979
Barreto97 <Barretolucas280@hotmail.com>
2025-05-28 12:29:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-// Simples script para animação na rolagem, se quiser depois pode melhorar
+// Script simples para animação na rolagem
 document.addEventListener('DOMContentLoaded', () => {
-  const animElements = document.querySelectorAll('.animate');
+  const animElements = document.querySelectorAll('section');
   animElements.forEach(el => {
     el.style.opacity = 1;
   });
