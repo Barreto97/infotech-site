22a3cd5570804ad678d512b7104938f87ea1d73c
Barreto97 <Barretolucas280@hotmail.com>
2025-05-28 16:45:57 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Script simples para animação na rolagem
-document.addEventListener('DOMContentLoaded', () => {
-  const animElements = document.querySelectorAll('section');
-  animElements.forEach(el => {
-    el.style.opacity = 1;
-  });
-});
